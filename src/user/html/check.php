a49cf624086782d0d9d6a0c145b63a7bbbf8b75c
yohei tanaka <yohei0910@keio.jp>
2022-05-01 22:38:16 +0900

    メール送信機能できた

--- src/user/html/check.php
+++ src/user/html/check.php
@@ -10,6 +10,7 @@
 <h2>確認画面</h2>
 <?php
 
+
 $dsn = 'mysql:host=db;dbname=shukatsu;charset=utf8;';
 $user = 'root';
 $password = 'password';
@@ -97,41 +98,22 @@ echo $email.'にメールで送りましたのでご確認ください。'."\n";
 $mail_sub = 'お問い合わせを受け付けました。';
 $mail_body = $name.'様、ご協力ありがとうございました。';
 $mail_body = html_entity_decode($mail_body,ENT_QUOTES,"UTF-8");
-$from = 'test@test.com';
+$from = 'yohei0910@softbank.ne.jp';
 $header="From: {$from}\nReply-To: {$from}\nContent-Type: text/plain;";
 
-echo $mail_body;
+
 mb_language('Japanese');
 mb_internal_encoding("UTF-8");
 
-if(mb_send_mail($email, $mail_sub, $mail_body,$header)) {
+if(mb_send_mail($email, $mail_sub, $mail_body,$header,"-f yohei0910@softbank.ne.jp")) {
   echo "メールを送信しました";
 } else {
   echo "メールの送信に失敗しました";
 };
 
-// if($name=='' || $email=='' || $address=='' || $phone=='' || $university=='' || $birthday==''){
-// 	echo '<p>未記入の項目があります。「<span class="deco">戻る</span>」ボタンをクリックしてください。</p>'."\n";
-// 	echo '<div class="submit">'."\n";
-// echo '<form>'."\n";
-// echo '<input type="button" onClick="history.back()" value="戻る">'."\n";
-// echo '</form>'."\n";
-// echo '</div>'."\n";
-// }else{
-// 	echo '<p>以上の内容を送信します。よろしければ「<span class="deco">送信</span>」ボタンを、修正する場合は「<span class="deco">戻る</span>」ボタンをクリックしてください。</p>'."\n";
-// 	echo '<div class="submit">'."\n";
-// echo '<form action="thanks.php" method="post">'."\n";
-// echo '<input type="hidden" name="name" value="'.$name.'">';
-// echo '<input type="hidden" name="email" value="'.$email.'">';
-// echo '<input type="hidden" name="message" value="'.$address.'">';
-// echo '<input type="hidden" name="message" value="'.$phone.'">';
-// echo '<input type="hidden" name="message" value="'.$university.'">';
-// echo '<input type="hidden" name="message" value="'.$birthday.'">';
-// echo '<input type="button" onClick="history.back()" value="戻る">'."\n";
-// echo '<input type="submit" value="送信">'."\n";
-// echo '</form>'."\n";
-// echo '</div>'."\n";
-// }
+
+echo phpinfo();
+
 ?>
 </div>
 </body>
